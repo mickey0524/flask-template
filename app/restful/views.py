dc9e3d4e43ff41c47a075ebf0edafd783ef631fa
baihao.01 <baihao.01@bytedance.com>
2017-11-01 15:43:32 +0800

    feat(flask): 增加不同的类型请求，修改run.py的执行权限

--- app/restful/views.py
+++ app/restful/views.py
@@ -1,10 +1,28 @@
 from flask import jsonify
+from flask import request as req
 from app.restful import restful
 
 @restful.route('/')
 def index():
-  obj = { 'baihao': 'shuai' }
+  obj = { 'baihao': 'ok' }
   return jsonify(
     message = "ok",
     data = obj
   )
+
+@restful.route('/<name>/', methods = ['GET', 'POST'])
+def param_index(name):
+  age = req.values.get('age')
+  return jsonify(
+    message = 'ok',
+    name = name,
+    age = age
+  )
+
+@restful.route('/raw_json', methods = ['POST'])
+def raw_json():
+  name = req.json['name']
+  return jsonify(
+    message = 'ok'
+  )
+
--- run.py
+++ run.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python
+import sys
+
 from app import create_app
+from setting import *
 
 app = create_app()
 
-app.run()
+if __name__ == '__main__':
+  port = int(sys.argv[1]) if len(sys.argv) > 1 else DEV_PORT
+  app.run(host = DEV_HOST, port = port)
